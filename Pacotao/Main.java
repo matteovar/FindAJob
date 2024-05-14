bd9460dc467efce708e7f3ebeffc3410572d0c82
matteovar <matvarnier@gmail.com>
2024-05-14 08:30:43 -0300

    update codes

--- Pacotao/Main.java
+++ Pacotao/Main.java
@@ -1,6 +1,11 @@
 // Source code is decompiled from a .class file using FernFlower decompiler.
 package JogaYJoga.Pacotao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
    public Main() {
    }
@@ -9,10 +14,41 @@ public class Main {
       new Pessoa("Jonatas", "@hotmail", "123");
       Cliente cliente1 = new Cliente("Jonatas", "@hotmail", "123", "aa");
       Locador locador1 = new Locador("Matteo", "@bol", "321", "bb");
+
       System.out.println(cliente1.getNome());
       System.out.println(cliente1.getEmail());
       System.out.println(cliente1.getSenha());
       System.out.println(cliente1.getCpf());
       System.out.println(locador1.getCnpj());
-   }
+
+       List<String> diasDisponiveis = new ArrayList<>();
+        diasDisponiveis.add("Segunda-feira");
+        diasDisponiveis.add("Terça-feira");
+        diasDisponiveis.add("Quarta-feira");
+        diasDisponiveis.add("Quinta-feira");
+        diasDisponiveis.add("Sexta-feira");
+        diasDisponiveis.add("Sábado");
+        diasDisponiveis.add("Domingo");
+
+        List<String> horariosDisponiveis = new ArrayList<>();
+        horariosDisponiveis.add("08:00");
+        horariosDisponiveis.add("10:00");
+        horariosDisponiveis.add("12:00");
+        horariosDisponiveis.add("14:00");
+        horariosDisponiveis.add("16:00");
+        horariosDisponiveis.add("18:00");
+        horariosDisponiveis.add("20:00");
+
+        Quadra quadra = new Quadra(diasDisponiveis, horariosDisponiveis);
+
+        // Exemplo de uso
+        System.out.println("Quadras disponíveis: " + quadra.cadastarQuadras());
+        quadra.selecionarQuadras("Quadra 1", "Segunda-feira", "10:00");
+        System.out.println("Horários disponíveis para a Quadra 1 na Segunda-feira: " + quadra.horariosDisponiveis("Quadra 1", "Segunda-feira"));
+        
+        System.out.println("Quadras disponíveis: " + quadra.cadastarQuadras());
+        quadra.selecionarQuadras("Quadra 1", "Segunda-feira", "10:00");
+        System.out.println("Horários disponíveis para a Quadra 1 na Segunda-feira: " + quadra.horariosDisponiveis("Quadra 1", "Segunda-feira"));
+
+      }
 }
--- Pacotao/Quadra.java
+++ Pacotao/Quadra.java
@@ -1,30 +1,65 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
 package JogaYJoga.Pacotao;
 
-public class Quadra {
-   public String[] listaOpcoes;
-   public String diasDisponiveis;
-   public String HorariosDisponiveis;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-   public Quadra() {
-   }
+class Quadra {
+    private List<String> listaOpcoes;
+    private Map<String, Map<String, List<String>>> quadras;
 
-   public String cadastarQuadras() {
-      throw new RuntimeException("Uncompilable source code - missing return statement");
-   }
+    public Quadra(List<String> diasDisponiveis, List<String> horariosDisponiveis) {
+        listaOpcoes = new ArrayList<>();
+        listaOpcoes.add("Quadra 1");
+        listaOpcoes.add("Quadra 2");
+        listaOpcoes.add("Quadra 3");
 
-   public String mostrarOpcoes() {
-      throw new RuntimeException("Uncompilable source code - missing return statement");
-   }
+        quadras = new HashMap<>();
+        for (String quadra : listaOpcoes) {
+            Map<String, List<String>> diasHorarios = new HashMap<>();
+            for (String dia : diasDisponiveis) {
+                diasHorarios.put(dia, new ArrayList<>(horariosDisponiveis));
+            }
+            quadras.put(quadra, diasHorarios);
+        }
+    }
 
-   public void selecionarQuadras() {
-   }
+    public String cadastarQuadras() {
+        return String.join(", ", listaOpcoes);
+    }
 
-   public String diasDisponiveis() {
-      throw new RuntimeException("Uncompilable source code - missing return statement");
-   }
+    public void selecionarQuadras(String quadra, String dia, String horario) {
+        if (quadras.containsKey(quadra)) {
+            Map<String, List<String>> diasHorarios = quadras.get(quadra);
+            if (diasHorarios.containsKey(dia)) {
+                List<String> horariosDisponiveis = diasHorarios.get(dia);
+                if (horariosDisponiveis.contains(horario)) {
+                    horariosDisponiveis.remove(horario);
+                    System.out.println("Horário reservado com sucesso para a " + quadra + " no dia " + dia + " às " + horario);
+                } else {
+                    System.out.println("O horário " + horario + " não está disponível para a " + quadra + " no dia " + dia);
+                }
+            } else {
+                System.out.println("Dia inválido");
+            }
+        } else {
+            System.out.println("Quadra inválida");
+            
+        }
+    }
 
-   public String horariosDisponiveis() {
-      throw new RuntimeException("Uncompilable source code - missing return statement");
-   }
+    public String horariosDisponiveis(String quadra, String dia) {
+        if (quadras.containsKey(quadra)) {
+            Map<String, List<String>> diasHorarios = quadras.get(quadra);
+            if (diasHorarios.containsKey(dia)) {
+                List<String> horariosDisponiveis = diasHorarios.get(dia);
+                return String.join(", ", horariosDisponiveis);
+            } else {
+                return "Dia inválido";
+            }
+        } else {
+            return "Quadra inválida";
+        }
+    }
 }
