f7c2342ba7916d80273111936011f8dd97c7dfbd
Matteo Varnier <80477283+matteovar@users.noreply.github.com>
2024-05-26 15:56:13 -0300

    Update

--- Aplicativo 2/src/main/java/JogaYJoga/Pacotao/Main.java
+++ Aplicativo 2/src/main/java/JogaYJoga/Pacotao/Main.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
     public static void clear() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
@@ -20,82 +21,86 @@ public class Main {
         }
     }
 
-    public static void operacoesCliente(Cliente cliente, ListaQuadras lista_quadras) {
+    public static void operacoesCliente(Cliente cliente, ListaQuadras listaQuadras) {
         Scanner teclado = new Scanner(System.in);
         int resposta;
 
-        System.out.println("Bem vindo de volta, " + cliente.getNome().toUpperCase());
-        System.out.println("\nSelecione uma opção: ");
-        System.out.println("[1] Voltar ao inicio \n[2] Alugar uma quadra");
+        while (true) {
+            System.out.println("Bem vindo de volta, " + cliente.getNome().toUpperCase());
+            System.out.println("\nSelecione uma opção: ");
+            System.out.println("[1] Voltar ao inicio \n[2] Alugar uma quadra");
 
-        resposta = teclado.nextInt();
-        while (resposta != 1 && resposta != 2) {
-            System.out.print("Digite uma opção válida: ");
             resposta = teclado.nextInt();
-        }
-
-        clear();
-        if (resposta == 2) {
-            System.out.println("--> Exibindo Lista de Quadras:");
-            lista_quadras.exibirNomeQuadras();
-
-            System.out.println("Escolha uma Quadra: ");
-            int numero_quadra = teclado.nextInt();
+            while (resposta != 1 && resposta != 2) {
+                System.out.print("Digite uma opção válida: ");
+                resposta = teclado.nextInt();
+            }
 
             clear();
-            System.out.println("-> Descrição: ");
-            lista_quadras.exibirDescricaoQuadras(numero_quadra);
-            System.out.println("Valor: ");
-            lista_quadras.exibirValor(numero_quadra);
-            System.out.println("[9] Confirmar [1] Voltar");
-            resposta = teclado.nextInt();
+            if (resposta == 1) {
+                menu(listaQuadras);
+            } else if (resposta == 2) {
+                System.out.println("--> Exibindo Lista de Quadras:");
+                listaQuadras.exibirNomeQuadras();
 
-            if (resposta == 9) {
-                ArrayList<LocalDate> diasDisponiveis = new ArrayList<>(lista_quadras.getQuadra(numero_quadra).getDiasHorarios().keySet());
-                if (diasDisponiveis.isEmpty()) {
-                    System.out.println("Não há dias disponíveis para esta quadra.");
-                } else {
-                    System.out.println("Dias disponíveis:");
-                    for (int i = 0; i < diasDisponiveis.size(); i++) {
-                        System.out.println("[" + (i + 1) + "] - " + diasDisponiveis.get(i));
-                    }
+                System.out.println("Escolha uma Quadra: ");
+                int numeroQuadra = teclado.nextInt();
 
-                    System.out.print("Escolha um dia: ");
-                    int diaEscolhido = teclado.nextInt() - 1;
-                    if (diaEscolhido >= 0 && diaEscolhido < diasDisponiveis.size()) {
-                        LocalDate data = diasDisponiveis.get(diaEscolhido);
-                        ArrayList<String> horarios = lista_quadras.getQuadra(numero_quadra).getHorariosDisponiveis(data);
-                        if (horarios.isEmpty()) {
-                            System.out.println("Não há horários disponíveis para esta data.");
-                        } else {
-                            System.out.println("Horários disponíveis para " + data + ":");
-                            for (int i = 0; i < horarios.size(); i++) {
-                                System.out.println("[" + (i + 1) + "] - " + horarios.get(i));
-                            }
+                clear();
+                System.out.println("-> Descrição: ");
+                listaQuadras.exibirDescricaoQuadras(numeroQuadra);
+                System.out.println("Valor: ");
+                listaQuadras.exibirValor(numeroQuadra);
+                System.out.println("[9] Confirmar [1] Voltar");
+                resposta = teclado.nextInt();
 
-                            System.out.print("Escolha um horário: ");
-                            int horarioEscolhido = teclado.nextInt() - 1;
-                            if (horarioEscolhido >= 0 && horarioEscolhido < horarios.size()) {
-                                String horarioString = horarios.get(horarioEscolhido);
-                                boolean reservado = lista_quadras.getQuadra(numero_quadra).reservarHorario(data, horarioString);
-                                if (reservado) {
-                                    System.out.println("Horário " + horarioString + " no dia " + data + " reservado com sucesso.");
-                                } else {
-                                    System.out.println("Falha ao reservar o horário. Tente novamente.");
-                                }
-                            } else {
-                                System.out.println("Opção de horário inválida.");
-                            }
-                        }
+                if (resposta == 9) {
+                    ArrayList<LocalDate> diasDisponiveis = new ArrayList<>(listaQuadras.getQuadra(numeroQuadra).getDiasHorarios().keySet());
+                    if (diasDisponiveis.isEmpty()) {
+                        System.out.println("Não há dias disponíveis para esta quadra.");
                     } else {
-                        System.out.println("Opção de dia inválida.");
+                        System.out.println("Dias disponíveis:");
+                        for (int i = 0; i < diasDisponiveis.size(); i++) {
+                            System.out.println("[" + (i + 1) + "] - " + diasDisponiveis.get(i));
+                        }
+
+                        System.out.print("Escolha um dia: ");
+                        int diaEscolhido = teclado.nextInt() - 1;
+                        if (diaEscolhido >= 0 && diaEscolhido < diasDisponiveis.size()) {
+                            LocalDate data = diasDisponiveis.get(diaEscolhido);
+                            ArrayList<String> horarios = listaQuadras.getQuadra(numeroQuadra).getHorariosDisponiveis(data);
+                            if (horarios.isEmpty()) {
+                                System.out.println("Não há horários disponíveis para esta data.");
+                            } else {
+                                System.out.println("Horários disponíveis para " + data + ":");
+                                for (int i = 0; i < horarios.size(); i++) {
+                                    System.out.println("[" + (i + 1) + "] - " + horarios.get(i));
+                                }
+
+                                System.out.print("Escolha um horário: ");
+                                int horarioEscolhido = teclado.nextInt() - 1;
+                                if (horarioEscolhido >= 0 && horarioEscolhido < horarios.size()) {
+                                    String horarioString = horarios.get(horarioEscolhido);
+                                    boolean reservado = listaQuadras.getQuadra(numeroQuadra).reservarHorario(data, horarioString);
+                                    if (reservado) {
+                                        System.out.println("Horário " + horarioString + " no dia " + data + " reservado com sucesso.");
+                                    } else {
+                                        System.out.println("Falha ao reservar o horário. Tente novamente.");
+                                    }
+                                } else {
+                                    System.out.println("Opção de horário inválida.");
+                                }
+                            }
+                        } else {
+                            System.out.println("Opção de dia inválida.");
+                        }
                     }
                 }
             }
         }
     }
 
-    public static void operacoesLocador(Locador locador, ListaQuadras lista_quadras) {
+    public static void operacoesLocador(Locador locador, ListaQuadras listaQuadras) {
         Scanner teclado = new Scanner(System.in);
         int resposta = 0;
         System.out.println("===*** Olá " + locador.getNome() + " ***===");
@@ -110,61 +115,78 @@ public class Main {
             }
 
             clear();
-            if (resposta == 2) {
+            if (resposta == 1) {
+                menu(listaQuadras);
+            } else if (resposta == 2) {
                 System.out.println("--> Exibindo locais:");
-                lista_quadras.exibirNomeQuadras();
-                if (lista_quadras.tamanho() == 0) {
+                listaQuadras.exibirNomeQuadras();
+                if (listaQuadras.tamanho() == 0) {
                     System.out.println("Não há locais cadastrados");
                 } else {
                     System.out.println("\nDigite o Número da quadra para alterar");
                     System.out.println("[1] Voltar");
                 }
             } else if (resposta == 3) {
-                System.out.println("--> Quadras Agendadas:");
-                if (lista_quadras.tamanho() == 0) {
-                    System.out.println("Você não possui nenhuma quadra agendada");
+                System.out.println("--> Verificando Agenda:");
+                if (listaQuadras.tamanho() == 0) {
+                    System.out.println("Você não possui nenhuma quadra cadastrada.");
                 } else {
-                    System.out.println("Atualmente você tem X alocações");
-                    System.out.println("Temos que trabalhar nesse método");
+                    for (int i = 0; i < listaQuadras.tamanho(); i++) {
+                        Quadra2 quadra = listaQuadras.getQuadra(i);
+                        System.out.println("Agenda para a quadra: " + quadra.getNome());
+                        quadra.getDiasHorarios().forEach((data, horarios) -> {
+                            System.out.println("Data: " + data);
+                            if (horarios.isEmpty()) {
+                                System.out.println("  Todos os horários estão reservados.");
+                            } else {
+                                System.out.println("  Horários disponíveis: " + horarios);
+                            }
+                        });
+                        System.out.println();
+                    }
                 }
             } else if (resposta == 4) {
                 Map<LocalDate, ArrayList<String>> diasHorarios = new HashMap<>();
                 ArrayList<String> horariosDia1 = new ArrayList<>();
+                ArrayList<String> horariosDia2 = new ArrayList<>();
                 horariosDia1.add("08:00 - 09:00");
                 horariosDia1.add("10:00 - 11:00");
-
+                horariosDia2.add("09:00 - 10:00");
+                horariosDia2.add("10:00 - 11:00");
                 diasHorarios.put(LocalDate.of(2024, 5, 31), horariosDia1);
+                diasHorarios.put(LocalDate.of(2024, 5, 30), horariosDia2);
 
-                Quadra2 quadraExemplo = new Quadra2("Quadra Futset Pinheiros", "Esta quadra é absurdamente legal", diasHorarios, "R$100.00");
-                lista_quadras.adicionarQuadras(quadraExemplo);
+                Quadra2 novaQuadra = new Quadra2("futset", "Quadra muito legal", diasHorarios, "110");
+                listaQuadras.adicionarQuadras(novaQuadra);;
 
-                System.out.println("Quadra: == " + quadraExemplo.getNome() + " == Adicionada");
+                System.out.println("Quadra: == " + novaQuadra.getNome() + " == Adicionada com sucesso");
             }
         }
-        menu(lista_quadras);
     }
 
-    public static void menu(ListaQuadras lista_quadras) {
+    public static void menu(ListaQuadras listaQuadras) {
         Cliente cliente1 = new Cliente("Jônatas", "XXX.XXX.XXX.XXX", "@bol", "120");
         Locador locador1 = new Locador("Beach Sports", "@gmail", "321", "bb");
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("------ JOGA Y JOGA Y --------");
-        int resposta = 0;
-        while (resposta != 1 && resposta != 2) {
-            System.out.print("Selecione uma opção: \n[1] Cliente [2] Locador\n--> ");
-            resposta = teclado.nextInt();
-        }
-        clear();
-        if (resposta == 1) {
-            operacoesCliente(cliente1, lista_quadras);
-        } else {
-            operacoesLocador(locador1, lista_quadras);
+        while (true) {
+            System.out.println("------ JOGA Y JOGA Y --------");
+            int resposta = 0;
+            while (resposta != 1 && resposta != 2) {
+                System.out.print("Selecione uma opção: \n[1] Cliente [2] Locador\n--> ");
+                resposta = teclado.nextInt();
+            }
+            clear();
+            if (resposta == 1) {
+                operacoesCliente(cliente1, listaQuadras);
+            } else {
+                operacoesLocador(locador1, listaQuadras);
+            }
         }
     }
 
     public static void main(String[] args) {
-        ListaQuadras lista_quadras = new ListaQuadras();
-        menu(lista_quadras);
+        ListaQuadras listaQuadras = new ListaQuadras();
+        menu(listaQuadras);
     }
 }
